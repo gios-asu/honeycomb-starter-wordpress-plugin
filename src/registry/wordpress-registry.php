924c5a43ec3790607760220a6e6b02b4ad2dc956
Nathan Rollins <nathan.rollins@asu.edu>
2017-10-26 16:42:34 -0700

    Add Admin Page to wordpress registry

--- src/registry/wordpress-registry.php
+++ src/registry/wordpress-registry.php
@@ -5,5 +5,6 @@
  */
 
 return [
-  HoneycombStarter\Shortcodes\Honeycomb_Starter_Shortcodes::class
+  HoneycombStarter\Shortcodes\Honeycomb_Starter_Shortcodes::class,
+  HoneycombStarter\Admin\Honeycomb_Starter_Admin_Page::class,
 ];
