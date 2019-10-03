ff84aa27528c407a14a52d9c4036614530b79836
Walter McConnell <Walter.McConnell@asu.edu>
2019-10-03 14:56:20 -0700

    Another Constant Update
    I forgot one instance of the version constant.

--- src/admin/gios-orcid-retriever-admin-page.php
+++ src/admin/gios-orcid-retriever-admin-page.php
@@ -3,7 +3,7 @@ namespace HoneycombStarter\Admin;
 use Honeycomb\Wordpress\Hook;
 
 // Avoid direct calls to this file
-if ( ! defined( 'HONEYCOMB_STARTER_PLUGIN_VERSION' ) ) {
+if ( ! defined( 'GIOS_ORCID_RETRIEVER_VERSION' ) ) {
   header( 'Status: 403 Forbidden' );
   header( 'HTTP/1.1 403 Forbidden' );
   exit();
